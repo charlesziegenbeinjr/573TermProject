44824f69e1a10140b098831910ac6ca5d6ab7466
Charles Ziegenbein Jr <cbzjr@umich.edu>
2022-12-05 14:14:07 -0500

    adding mental health algorithm run

--- sw_explainable.py
+++ sw_explainable.py
@@ -1,6 +1,3 @@
-
-
-
 import numpy as np
 import pandas as pd
 
@@ -11,22 +8,22 @@ from sklearn.cluster import KMeans
 from pyJoules.energy_meter import measure_energy
 
 
-@measure_energy
+# @measure_energy
 def main():
-    frame = pd.read_csv(r"iris.csv")
+    frame = pd.read_csv(r"Mental_Health_Cleaned.csv")
 
-    X = frame.drop('variety', axis=1)
+    X = frame.drop('MH1', axis=1)
 
-    for cols in X.columns:
-        X[cols] = X[cols].astype(float)
-        k1 = X[cols].mean()
-        k2 = np.std(X[cols])
-        X[cols] = (X[cols] - k1)/k2
-    k = 3
+    # for cols in X.columns:
+    #     X[cols] = X[cols].astype(float)
+    #     k1 = X[cols].mean()
+    #     k2 = np.std(X[cols])
+    #     X[cols] = (X[cols] - k1)/k2
+    k = 14
     kmeans = KMeans(k, random_state=43)
     kmeans.fit(X)
     p = kmeans.predict(X)
-    class_names = np.array(['Setosa', 'Versicolor', 'Virginica'])
+    class_names = np.array(['0','1', '2', '3','4','5','6','7','8','9','10','11','12','13'])
 
 
     tree = Tree(k=k)
