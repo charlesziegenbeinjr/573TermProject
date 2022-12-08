d4dbaa344f3f2177c0d09bda4a936a14524a0978
Charles Ziegenbein Jr <cbzjr@umich.edu>
2022-12-08 00:01:34 -0500

    Added ExKMC directory

--- sw_explainable.py
+++ sw_explainable.py
@@ -1,41 +1,42 @@
 import numpy as np
 import pandas as pd
 
-from ExKMC.Tree import Tree
+from ExplainableKMC import Tree
+# from ExKMC.Tree import Tree
 from IPython.display import Image
 from sklearn.cluster import KMeans
+import time
 
 from pyJoules.energy_meter import measure_energy
 
 
 # @measure_energy
 def main():
-    frame = pd.read_csv(r"Mental_Health_Cleaned.csv")
+    start = time.time()
+    frame = pd.read_csv(r"Data/Mental_Health_Cleaned.csv")
 
     X = frame.drop('MH1', axis=1)
 
-    # for cols in X.columns:
-    #     X[cols] = X[cols].astype(float)
-    #     k1 = X[cols].mean()
-    #     k2 = np.std(X[cols])
-    #     X[cols] = (X[cols] - k1)/k2
+    kmeans_start = time.time()
     k = 14
     kmeans = KMeans(k, random_state=43)
     kmeans.fit(X)
-    p = kmeans.predict(X)
-    class_names = np.array(['0','1', '2', '3','4','5','6','7','8','9','10','11','12','13'])
+    k_means_finish = time.time() - kmeans_start
+    print("KMeans Execution Time: %f" % k_means_finish)
+    
+    clusters = kmeans.cluster_centers_
+    
 
-
-    tree = Tree(k=k)
-    tree.fit(X, kmeans)
+    start_ExKMC = time.time()
+    tree = Tree.Tree(k=k)
+    tree.fit(X, clusters, kmeans)
     tree.plot(filename="test", feature_names=X.columns)
+    finish_ExKMC = time.time() - start_ExKMC
+    print("ExKMC Execution Time: %f" % finish_ExKMC)
+    
+    finish = time.time() - start
+    print("Total Execution Time: %f " % finish)
     Image(filename='test.gv.png')
-
-    tree = Tree(k=k, max_leaves=6)
-    tree.fit(X, kmeans)
-    tree.plot(filename="test1", feature_names=X.columns)
-    Image(filename='test1.gv.png')
-
 
 if __name__ == "__main__":
     main()
